c13503e0b3dbfae159f5ebbabc265192b09f6bc1
boris jing <115875711+ellegmb@users.noreply.github.com>
2023-01-18 15:52:39 +0400

    Update index.js

--- calculator/index.js
+++ calculator/index.js
@@ -1,53 +1,158 @@
-let input = "";
-let first = input;
+var cal_screen="" ;
+
+var input_one ;
+var input_two ;
+let operation ;
+let result ;
+
+// Funtions for evaluation of the diffrent operations
+// returns: evaluated result
+
+function add(x,y){
+   sol = x + y;
+   return toString(sol);
+ }
+function subtract(x,y){
+  return x - y;
+}
+function multiply(x,y){
+  return x * y;
+}
+function divide(x,y){
+  result = x / y;
+  result = Math.round(result * 10) / 10;
+  return result;
+}
+function add(x,y) {
+  return x+y;
+} 
+
+//  equal button implementation for diffent oprator results
+
+document.getElementById("equal").onclick = function () {
+  if (operation == "+"){
+  input_two = Number(cal_screen);
+  console.log(operation);  
+  console.log(input_one);
+   cal_screen = add(input_one,input_two);
+  }
+  else if(operation == "-") {
+    input_two = Number(cal_screen);
+    console.log(operation);  
+    console.log(input_one);
+     cal_screen = subtract(input_one,input_two);
+  }
+  else if(operation == "x") {
+    input_two = Number(cal_screen);
+    console.log(operation);  
+    console.log(input_one);
+     cal_screen = multiply(input_one,input_two);
+  }
+  else if(operation == "/") {
+    input_two = Number(cal_screen);
+    console.log(operation);  
+    console.log(input_one);
+     cal_screen = divide(input_one,input_two);
+  }
+  else{
+    cal_screen = "Error"
+  }
+  document.getElementById("screeninput").innerHTML =cal_screen;
+}
+
+
+// decleration of diffrent numerical buttons bottuns
+
 
 document.getElementById("num0").onclick = function () {
-  input += "0";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "0";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("num1").onclick = function () {
-  input += "1";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "1";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("num2").onclick = function () {
-  input += "2";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "2";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("num3").onclick = function () {
-  input += "3";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "3";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("num4").onclick = function () {
-  input += "4";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "4";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("num5").onclick = function () {
-  input += "5";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "5";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("num6").onclick = function () {
-  input += "6";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "6";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("num7").onclick = function () {
-  input += "7";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "7";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("num8").onclick = function () {
-  input += "8";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "8";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("num9").onclick = function () {
-  input += "9";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += "9";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 document.getElementById("dot").onclick = function () {
-  input += ".";
-  document.getElementById("screeninput").innerHTML = input;
-};
-document.getElementById("clear").onclick = function () {
-  input = "";
-  document.getElementById("screeninput").innerHTML = input;
-};
+  cal_screen += ".";
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
 
-console.log(first);
+
+document.getElementById("clear").onclick = function () {
+  
+  cal_screen = "";
+  input_one = 0 ;
+  input_two = 0 ;
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
+
+//decleration for diffrent operator buttons
+
+
+document.getElementById("divide").onclick = function () {
+  operation = "/";
+  input_one = Number(cal_screen);
+  cal_screen = '';
+   
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
+document.getElementById("multiply").onclick = function () {
+  operation = "x";
+  input_one = Number(cal_screen);
+  cal_screen = '';
+   
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
+document.getElementById("subtract").onclick = function () {
+  operation = "-";
+  input_one = Number(cal_screen);
+  cal_screen = '';
+   
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
+
+document.getElementById("add").onclick = function () {
+  operation = "+";
+  input_one = Number(cal_screen);
+  cal_screen = '';
+   
+  document.getElementById("screeninput").innerHTML = cal_screen;
+}
+
+
+
+
+
+
