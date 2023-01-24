df8a438f7956c7b7d41e9f1b8eb3845e29947c9a
boris jing <115875711+elleboriz@users.noreply.github.com>
2023-01-25 01:29:57 +0400

    Update PE_functions.py
    imported regex  file funtionalities

--- POWER EMAIL AUTOMATE/PE_functions.py
+++ POWER EMAIL AUTOMATE/PE_functions.py
@@ -1,14 +1,13 @@
-
+import regex
 """
 OPENING TEXT FILE AND READING CONTENT
 :returns FILE CONTENT
 
 """
-
-
+#input('Enter file PATH:')
 def file_open():
     try:
-        with open(f"r{input('Enter file PATH:')}",errors='replace') as opened_file:
+        with open(r"C:\Users\gmb\Desktop\Good.txt",errors='replace') as opened_file:
             r_file = opened_file.read()
             opened_file.close()
             return r_file
@@ -17,10 +16,16 @@ def file_open():
                         TRY THESE TO TROUBLESHOOT:
                         * Verify Your File PATH if its correct
                         * Verify your File name
-                        * Verify your File Type e.g(.txt)
+                        * Verify your File Type (.txt)
                         """)
 
-
 read_file = file_open()
+
+"""  STILL UNDER DEVELOPEMENT
+test = regex.regex_filter(read_file)
+
+"""
+
+
 
 
