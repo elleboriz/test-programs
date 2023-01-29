0958f1c9ef5f40cde9024d3f5ab6698a3d0b129e
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-01-30 03:35:55 +0400

    bug fixes
    bug fixes

--- POWER EMAIL AUTOMATE/PE_functions.py
+++ POWER EMAIL AUTOMATE/PE_functions.py
@@ -1,60 +1,33 @@
-import re
+
+from regex import Email
 
 """
-extracts emails from string text and filter out all emails in it,
-return: List of extracted emails 
+OPENING TEXT FILE AND READING CONTENT
+:returns FILE CONTENT
+"""
+def file_open():
+    try:
+        with open(input('Enter file PATH:'),errors='replace') as opened_file:
+            r_file = opened_file.read()
+            opened_file.close()
+            return r_file
+    except:
+        print(Exception ,"""OOPS.. AN ERROR OCCURRED 
+                        TRY THESE TO TROUBLESHOOT:
+                        * Verify Your File PATH if its correct
+                        * Verify your File name
+                        * Verify your File Type (.txt)
+                        """)
+
+data =file_open()
+in_file = Email(data)
+
+
+"""example
+
+print(in_file.match_domain())
 """
 
-
-class Email:
-    def __init__(self, file_data: str):
-        assert len(file_data) > 1, f"input file must contain data"
-        self.email = file_data
-
-    def find_emails(self):
-
-        """
-        extracts emails from string text using regex pattern and filter them out ,
-        :return: List of extracted emails
-
-        """
-        try:
-            pattern = re.compile(r'([a-zA-Z0-9-._]+)@([a-zA-Z-]+)\.([a-zA-Z]+)')
-            matches = pattern.finditer(self.email)
-            result = [match.group(0) for match in matches]
-            return "\n".join(result)
-        except:
-            return print(Exception, "Oops An Error occurred ")
-
-    def match_domain(self):
-
-        domains = input(
-            """Enter a Domain Or a list of Domains e.g(@gmail.com , @yahoo.com etc) you want to filter out: """
-        )
-        assert len(domains) > 0, f"{domains} must contain 1 or more domain name for search match cases "
-        domains = domains.split(',')
-
-        """
-        filters out  emails of chosen domain name or names and appending to results
-        
-        """
-        results = []
-        for domain in domains:
-            domain = domain.strip()
-
-            data = self.email.replace("\n", ',').split(',')
-
-            result = set(filter(lambda email: domain in email, data))
-            results.append(result)
-
-        """
-        collecting the results sets (non duplicate) converting to list and joining 
-        
-        return: match emails 
-        """
-
-        match_list = [list(i) for i in results]
-        final_result = []
-        for match in match_list:
-            final_result += match
-        return "\n".join(final_result)
+"""
+By elleBoriz
+"""
